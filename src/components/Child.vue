8fa53c1b9f4a61e7d2692a627f346e8a7c8c3b19
Viktor Volkov <144665914+vollmond08@users.noreply.github.com>
2023-09-11 09:14:59 +0000

    Update 3 files

---
+++ src/components/Child.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{title}}
+    <ul>
+        <li v-for="item in list" :key="item.id">
+            {{item.id}}: {{item.name}}
+        </li>
+    </ul>
+    {{options.isCorrect}}
+  </div>
+</template>
+
+<script lang="ts" setup>
+    type Item = {
+        id: number;
+        name: string;
+    }
+
+    defineProps<{
+        title: string;
+        list: Item[];
+        options: {
+            isCorrect: boolean;
+        }
+    }>()
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
-    {{test}}
+    <Child title="child1" :list="[{id: 1, name: 'item1'}]" :options="{isCorrect:false}" />
+    <hr />
+    <Child title="child2" :list="[{id: 1, name1: 'item1'}]" :options="{}" />
   </div>
 </template>
 
 <script lang="ts" setup>
-const test = 'test'
+import Child from './Child.vue'
+
 </script>
